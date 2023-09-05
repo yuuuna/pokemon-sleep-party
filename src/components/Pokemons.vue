57b312a76510ab9b5913f8edd7d4b239538775f1
Yuuna <k9532121@gmail.com>
2023-09-05 16:32:59 +0800

    feat: 🎸 圖鑑新增食材篩選

--- src/components/Pokemons.vue
+++ src/components/Pokemons.vue
@@ -2,9 +2,11 @@
 import { ref } from 'vue'
 import pokemons from '@/json/pokemons.json';
 import fruits from '@/json/fruits.json';
+import ingredients from '@/json/ingredients.json';
 
 let filterData = ref({
-    "fruits": []
+    "fruits": [],
+    "ingredients": []
 })
 let pokemonList = ref([]);
 filterPokemon();
@@ -23,15 +25,36 @@ function filterFruit(fruitId) {
     filterPokemon();
 }
 
+function filterIngredient(ingredientId) {
+    let ingredientList = filterData.value.ingredients;
+    if (ingredientList.includes(ingredientId)) {
+        // 有找到，移除
+        let index = ingredientList.findIndex((id) => id === ingredientId);
+        ingredientList.splice(index, 1);
+    } else {
+        // 沒找到，新增
+        ingredientList.push(ingredientId);
+    }
+    filterPokemon();
+}
+
 function filterPokemon() {
-    if (filterData.value.fruits.length === 0) {
+    if (filterData.value.fruits.length === 0 && filterData.value.ingredients.length === 0) {
         pokemonList.value = pokemons;
         return;
     }
     let pokemonTempData = pokemons;
-    pokemonTempData = pokemonTempData.filter((pokemon) => filterData.value.fruits.includes(pokemon.fruit_id));
-    pokemonList.value = pokemonTempData;
 
+    if (filterData.value.fruits.length > 0) {
+        pokemonTempData = pokemonTempData.filter((pokemon) => filterData.value.fruits.includes(pokemon.fruit_id));
+    }
+
+    if (filterData.value.ingredients.length > 0) {
+        console.log('in');
+        pokemonTempData = pokemonTempData.filter((pokemon) => pokemon.ingredients_id.some((ingre) => filterData.value.ingredients.includes(ingre)));
+    }
+    
+    pokemonList.value = pokemonTempData;
 }
 
 </script>
@@ -49,10 +72,21 @@ function filterPokemon() {
             <div class="row">
                 <div class="col-2">樹果</div>
                 <div class="col-10 fruit-filter">
-                    <div v-for="fruit in fruits" class="fruit-card" :class="{ clicked : filterData.fruits.includes(fruit.id) }"
-                        @click="filterFruit(fruit.id)">
+                    <div v-for="fruit in fruits" class="fruit-card"
+                        :class="{ clicked: filterData.fruits.includes(fruit.id) }" @click="filterFruit(fruit.id)">
                         <span class="fruit-title">{{ fruit.name }}</span>
                         <img class="fruit-img pt-1 pb-1" :src="`./images/fruits/${fruit.id}.png`" />
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-2">食材</div>
+                <div class="col-10 fruit-filter">
+                    <div v-for="ingredient in ingredients" class="fruit-card"
+                        :class="{ clicked: filterData.ingredients.includes(ingredient.id) }"
+                        @click="filterIngredient(ingredient.id)">
+                        <span class="fruit-title">{{ ingredient.name }}</span>
+                        <img class="fruit-img pt-1 pb-1" :src="`./images/ingredients/${ingredient.id}.png`" />
                     </div>
                 </div>
             </div>
@@ -79,7 +113,12 @@ function filterPokemon() {
                     <img class="fruit-img" :src="`./images/fruits/${pokemon.fruit_id}.png`" />
                     {{ pokemon.fruit }}
                 </td>
-                <td>{{ pokemon.ingredients }}</td>
+                <td>
+                    <span v-for="(ingredient, index) in pokemon.ingredients">
+                        {{ (index > 0 ? "、" : "") }} <img class="fruit-img"
+                            :src="`./images/ingredients/${ingredient.id}.png`" /> {{ ingredient.name }}
+                    </span>
+                </td>
                 <td>{{ pokemon.place }}</td>
                 <td>{{ pokemon.skill }}</td>
                 <td><span class="badge text-bg-primary">{{ pokemon.expertise }}</span></td>
@@ -100,6 +139,7 @@ function filterPokemon() {
     display: flex;
     flex-direction: column;
     align-items: center;
+
     &.clicked {
         background-color: #b0ddff;
         border: #8bb5d4 solid 1px;
